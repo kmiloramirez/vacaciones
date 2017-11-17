7d1643c52d677d0395504de4ef1bbdf57f3f6d3d
kmiloramirez <kmiloperlaza1996@gmail.com>
2017-11-17 08:36:47 -0500

    Initial commit

---
+++ src/main/java/com/ceiba/vacaciones/VacacionesApplication.java
@@ -0,0 +1,12 @@
+package com.ceiba.vacaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacacionesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ceiba/vacaciones/VacacionesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ceiba.vacaciones;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VacacionesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
