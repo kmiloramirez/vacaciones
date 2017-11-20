00d54e21c4b4305c88dcc453829bec8f046aebd8
kmiloramirez <kmiloperlaza1996@gmail.com>
2017-11-20 08:05:13 -0500

    creacion del domino

--- src/main/java/dominio/SolicitudVacaciones.java
+++ src/main/java/dominio/SolicitudVacaciones.java
@@ -1,5 +1,41 @@
 package dominio;
 
+import java.util.Calendar;
+
 public class SolicitudVacaciones {
+	
+	private Calendar fechaDeSolicitudDeinicio;
+	private Calendar fechaDeSolicitudFin;
+	private Calendar fechaDeRegreso;
+	private int CantidadDeDias;
+	private String jefeInmediato;
+	public Calendar getFechaDeSolicitudDeinicio() {
+		return fechaDeSolicitudDeinicio;
+	}
+	public Calendar getFechaDeSolicitudFin() {
+		return fechaDeSolicitudFin;
+	}
+	public Calendar getFechaDeRegreso() {
+		return fechaDeRegreso;
+	}
+	public int getCantidadDeDias() {
+		return CantidadDeDias;
+	}
+	public String getJefeInmediato() {
+		return jefeInmediato;
+	}
+	public SolicitudVacaciones(Calendar fechaDeSolicitudDeinicio, Calendar fechaDeSolicitudFin, Calendar fechaDeRegreso,
+			int cantidadDeDias, String jefeInmediato) {
+		this.fechaDeSolicitudDeinicio = fechaDeSolicitudDeinicio;
+		this.fechaDeSolicitudFin = fechaDeSolicitudFin;
+		this.fechaDeRegreso = fechaDeRegreso;
+		CantidadDeDias = cantidadDeDias;
+		this.jefeInmediato = jefeInmediato;
+	}
+	
+	public SolicitudVacaciones(int cantidadDeDias) {
+		CantidadDeDias = cantidadDeDias;
+	}
+	
 
 }
